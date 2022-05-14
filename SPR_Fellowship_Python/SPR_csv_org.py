9625cd97903aff2d46e67b8008759f2005220fb8
Sam_E_7743 <64446365+samshott@users.noreply.github.com>
2022-05-14 11:44:19 -0700

    Python: fix copying file copying functions- add docstrings, copy all txt to folder

--- SPR_Fellowship_Python/SPR_csv_org.py
+++ SPR_Fellowship_Python/SPR_csv_org.py
@@ -1,6 +1,4 @@
 import os
-import parser
-
 import hoboreader
 import pandas as pd
 import shutil
@@ -26,22 +24,32 @@ txt_paths = search_filetype(path = "./OneDrive_1_5-6-2022/",extentension=".txt")
 csv_paths = search_filetype(extentension=".csv")
 
 
-def copy_file(search_path=".", copy_dir="", extentension=""):
+def copy_file(search_path=".", copy_dir="", extentension="",show_errors = True):
+    """
+    :param search_path: string, directory to search
+    :param copy_dir: string, directory to copy files to
+    :param extentension: string, type of files to copy - include the '.'
+    :param show_errors: bool, whether to print files that did not copy succesfully or not
+    :return: a list of files that did not copy successfully
+    """
+    error_files = []
     for dirpath, dirnames, filenames in os.walk(search_path):
         for filename in filenames:
             if filename[-len(extentension):] == extentension:
-                shutil.copyfile(src=dirpath+filename,
-                                dst=copy_dir+filename,
-                                follow_symlinks=False)
+                full_path = dirpath+"/"+filename
+                try:
+                    shutil.copy2(src=full_path,dst=copy_dir+filename,follow_symlinks=False)
+                except:
+                    error_files.append(full_path)
+    if show_errors:
+        print(error_files)
+    return (error_files)
 
-copy_file(search_path=os.getcwd()+"/OneDrive_1_5-6-2022/",
-          copy_dir= os.getcwd()+"/LegacyFiles/All_txt/",
+
+copy_file(search_path="./OneDrive_1_5-6-2022/",
+          copy_dir= "./LegacyFiles/All_txt/",
           extentension=".txt")
 
-for txt_path in txt_paths:
-    shutil.copyfile(txt_path , "./LegacyFiles/All_txt/")
-
-print(csv_paths[1:5])
 
 
 
