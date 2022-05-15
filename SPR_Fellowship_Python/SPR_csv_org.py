0d5f2b89243860ca531fe92438d69b555db8ef8e
Sam_E_7743 <64446365+samshott@users.noreply.github.com>
2022-05-14 20:39:23 -0700

    Python: made a function for combining data.frames - needs work (see comments)

--- SPR_Fellowship_Python/SPR_csv_org.py
+++ SPR_Fellowship_Python/SPR_csv_org.py
@@ -2,6 +2,7 @@ import os
 import hoboreader
 import pandas as pd
 import shutil
+import matplotlib as mp
 
 os.chdir("..")
 os.getcwd()
@@ -40,15 +41,46 @@ def copy_file(search_path=".", copy_dir="", extentension="",show_errors = True):
         print(error_files)
     return (error_files)
 
+def combine_txt_df(path_list = [], sep = "\t", quiet=False):
+    """
+    :param path_list: list, paths to txt files
+    :param sep: string, how files are parsed
+    :return: list, index 0: pandas data.frame of appended files, index 1: error files
+    """
+    append_df=pd.DataFrame()
+    error_files=[]
+    for txt_file in path_list:
+        try:
+            txt_df = pd.read_csv(txt_file, sep=sep)
+            for column in txt_df.columns:
+                if not (column in append_df.columns):
+                    append_df[str(column)] = ""
+            append_df = append_df.append(txt_df)
+        except:
+            error_files.append(txt_file)
+    if not quiet:
+        print("Unsuccesfull appends: ", error_files)
+    return append_df
+#still needed: - columns to title case
+#              - whats wrong with 10/15/2005 0:53 file?
+
+
 
 # copy_file(search_path="./OneDrive_1_5-6-2022/",
 #           copy_dir= "./LegacyFiles/All_txt/",
 #           extentension=".txt")
 
+list_txt = search_filetype(path="LegacyFiles/All_txt/", extentension=".txt")
 
 
 
 
+test_txt_df1 = pd.read_csv(list_txt[1], sep="\t")
+test_txt_df2 = pd.read_csv(list_txt[2], sep="\t")
+
+test_txt_df["Station"] = "1"
+
+test_app_df = combine_txt_df(list_txt[0:50])
 
 
 
